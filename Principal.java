455d1b16b325e771c7afc3347a9588b317efe6c1
Gederson Marcos <98136251+gmsilvera@users.noreply.github.com>
2023-04-04 23:31:24 +0000

    exercicio1

---
+++ Principal.java
@@ -0,0 +1,15 @@
+
+
+public class Principal {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+      System.out.println("Hello World");
+      int i = 0;
+      while (i < 5) {
+        System.out.println(i);
+        i++;
+      }
+    }
+  }
